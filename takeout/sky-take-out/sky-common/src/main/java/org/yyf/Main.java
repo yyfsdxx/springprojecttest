0809ff3a365514be19b54c0d6f5fe935af314bff
yyf <807199239@qq.com>
2025-07-31 19:53:23 +0800

    merge initproject

---
+++ takeout/sky-take-out/sky-common/src/main/java/org/yyf/Main.java
@@ -0,0 +1,13 @@
+package org.yyf;
+
+/**
+ * @author yufengyang
+ * @Package org.yyf
+ * @date ${DATE} ${TIME}
+ * @school hnist
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ takeout/sky-take-out/sky-pojo/src/main/java/org/yyf/vo/OrderVO.java
@@ -0,0 +1,25 @@
+package org.yyf.vo;
+
+import org.yyf.entity.OrderDetail;
+import org.yyf.entity.Orders;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.yyf.entity.OrderDetail;
+import org.yyf.entity.Orders;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderVO extends Orders implements Serializable {
+
+    //订单菜品信息
+    private String orderDishes;
+
+    //订单详情
+    private List<OrderDetail> orderDetailList;
+
+}
